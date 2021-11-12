2bf9672e02f7d2c90d9ae7d5683a4285b7a4131a
Walid Wazini <wazini25@gmail.com>
2021-11-13 04:50:45 +0800

    intital provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 
 import './screens/Screen_Home.dart';
-
+import 'provider/TasksProvider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,12 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color(0xffc11430),
-        scaffoldBackgroundColor: Colors.white70,
+    return ChangeNotifierProvider(
+      create: (context) => TasksProvider(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Color(0xffc11430),
+          scaffoldBackgroundColor: Colors.white70,
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
