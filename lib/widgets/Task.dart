6886139dea602e0eb21fa068ad968a54861bc74c
Walid Wazini <wazini25@gmail.com>
2021-11-13 08:23:44 +0800

    updates

--- lib/widgets/Task.dart
+++ lib/widgets/Task.dart
@@ -10,15 +10,41 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-          activeColor: Theme.of(context).primaryColor,
-          checkColor: Colors.white,
-          value: task.isDone,
-          onChanged: (_){},
-        )
-      ],
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Checkbox(
+            activeColor: Theme.of(context).primaryColor,
+            checkColor: Colors.white,
+            value: task.isDone,
+            onChanged: (_){},
+          ),
+          SizedBox(width: 20,),
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                task.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 22,
+                ),
+              ),
+             if (task.description.isNotEmpty)
+               Container(
+                 child: Text(
+                   task.description,
+                   style: TextStyle(fontSize: 18, height: 1.5),
+                 ),
+               )
+
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
