ce2ac9371eaa6f07d216bb230dd7064b71d6e831
Walid Wazini <wazini25@gmail.com>
2021-11-26 21:10:56 +0800

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 
 import './screens/Screen_Home.dart';
 import 'provider/TasksProvider.dart';
-void main() => runApp(MyApp());
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
 //  const MyApp({Key? key}) : super(key: key);
